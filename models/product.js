e8e6c527083ea467392a3d720804a745be187358
Daria Hryshchenko <daria199210@gmail.com>
2023-06-08 04:39:40 +0300

    add fild amount

--- models/product.js
+++ models/product.js
@@ -22,6 +22,10 @@ const productSchema = new Schema(
       type: String,
       required: true,
     },
+    amount: {
+      type: String,
+      required: true,
+    },
   },
   { versionKey: false, timestamps: true }
 );
